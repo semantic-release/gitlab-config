09e4678e2da271964bc40a6a5469c869b15dab14
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-16 21:06:09 -0500

    feat: support semantic-release@14.0.0

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
   verifyConditions: ['@semantic-release/npm', '@semantic-release/gitlab'],
   publish: ['@semantic-release/npm', '@semantic-release/gitlab'],
+  success: false,
+  fail: false,
 };
