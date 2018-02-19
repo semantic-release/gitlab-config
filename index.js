ceb00cf25fd39ee7e763f1fb2bea05502c23e73a
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-19 01:02:48 -0500

    feat: semantic-release@15.0.0
    BREAKING CHANGE: require semantic-release@15.0.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   verifyConditions: ['@semantic-release/npm', '@semantic-release/gitlab'],
+  prepare: ['@semantic-release/npm'],
   publish: ['@semantic-release/npm', '@semantic-release/gitlab'],
   success: false,
   fail: false,
