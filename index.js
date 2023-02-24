3647ae9c5bb71fd5c47b36c26c2f3dd594a2d430
Florian Greinacher <florian.greinacher@siemens.com>
2023-02-24 17:45:18 +0100

    feat: convert package to ESM
    BREAKING CHANGES:
    
    - The plugin is not compatible with semantic-release < 20.1.0
    - This package is now ESM-only and cannot be used by CJS packages.
    - The minimum supported Node version has been changed to 18.0.0.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/gitlab',
-  ],
-};
+export const plugins = [
+  "@semantic-release/commit-analyzer",
+  "@semantic-release/release-notes-generator",
+  "@semantic-release/npm",
+  "@semantic-release/gitlab",
+];
