845db703e5af895a1768cdd2eaecd66a110c3d68
Florian Greinacher <florian@greinacher.de>
2023-04-11 12:58:55 +0200

    style: use double quotes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/gitlab',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/gitlab",
   ],
 };
